961723e779af23f3be9d91638230d397392954a7
Saad <smblackeye@gmail.com>
2023-06-20 08:28:17 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main prints _putchar followed by a new line
- * Return : Always 0 (success)
+ * main - prints _putchar followed by a new line
+ * Return: Always 0 (success)
  */
 
 int main(void)
