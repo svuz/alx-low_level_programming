e6d26fcdb743d3695befe0553a3e65bcae4e0b12
Saad <smblackeye@gmail.com>
2023-07-05 19:46:45 -0700

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _print_rev_recursion - print reversed string
- * *s: string
+ * @s: string
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-	 _print_rev_recursion(s + 1);
-	 _putchar(*s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
