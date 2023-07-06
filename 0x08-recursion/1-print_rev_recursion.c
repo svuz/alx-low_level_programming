273286e44331d80a7fcd5ef9599eca94f2871fa8
Saad <smblackeye@gmail.com>
2023-07-05 19:44:46 -0700

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,11 @@
  * %s: string
  */
 
-void _print_rev_recursion (char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-	 _print_rev_recursion (s+1);
-	 _putchar (*s);
+	 _print_rev_recursion(s + 1);
+	 _putchar(*s);
 	}
 }
