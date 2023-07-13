c87bead0504643de9e76f9e750941602e2ac00c4
Saad <smblackeye@gmail.com>
2023-07-13 03:05:52 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
+
 /*
  * malloc_checked - allocate memory using malloc
  * b - number of bytes to allow to allocate 
- * return - a pointer to the allocated mem*/
+ * return - a pointer to the allocated mem
+ * */
+
 void *malloc_checked(unsigned int b)
 {
         void *ptr;
