4d309c3d4364888bc1f25faba3802dff6615cff8
Saad <smblackeye@gmail.com>
2023-07-13 03:41:18 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,20 +3,20 @@
 
 /*
  * malloc_checked - allocate memory using malloc
- * b - number of bytes to allow to allocate 
+ * @b - number of bytes to allow to allocate 
  * return - a pointer to the allocated mem
- * */
+ * 
+*/
 
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
-	
+
 	ptr = malloc(b);
 	
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	
-	return ptr;
+	return (ptr);
 }
